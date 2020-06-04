06808db6a74d402c500a0ecac83aa8c75d5e62a8
zhangchuan <zc12436108@outlook.com>
2020-06-04 18:25:52 +0800

    docker springmvc

---
+++ comsume_server/src/main/java/com/chuan/demo/thread/InheritableThreadLocalTest.java
@@ -0,0 +1,10 @@
+package com.chuan.demo.thread;
+
+/**
+ * @ClassName InheritableThreadLocalTest
+ * @Description: TODO
+ * @Author: wanda
+ * @Date 2020/6/2 17:22
+ **/
+public class InheritableThreadLocalTest {
+}
---
+++ springmvc_demo/src/main/java/com/chuan/demo/config/SpringConfig.java
@@ -0,0 +1,10 @@
+package com.chuan.demo.config;
+
+/**
+ * @ClassName SpringConfig
+ * @Description: TODO
+ * @Author: wanda
+ * @Date 2020/6/4 17:24
+ **/
+public class SpringConfig {
+}
